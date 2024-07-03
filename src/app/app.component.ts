b2082366d033cb877d790b636a82e32854468b4a
Gerold <g.lindorfer@dal.de>
2024-07-03 18:18:55 +0200

    in work: mixing buttons ans routings in navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
 import { ConfigFileService } from './shared/config-file.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from "rxjs"
 
+const greyOut = "color: lightgrey;"
+type IsEnabled = {
+  upload:   boolean,
+  edit:     boolean,
+  run:      boolean,
+  download: boolean,
+  discard:  boolean
+}
 
 @Component({
   selector: 'app-root',
@@ -10,13 +19,70 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AppComponent {
   title = "lonely-lobster"
-  filename: string
+  filename: string = ""
 
-  public showEditRunSaveOptions = false
-
+  public canRunDownloadDiscard = false
+  
   constructor(
     private router: Router,
     private route:  ActivatedRoute, 
     private cfs:    ConfigFileService) { }
 
+  public runDownloadDiscardColor(): string | undefined {
+    return this.canRunDownloadDiscard ? undefined : greyOut 
+  }  
+
+  public discard(): void {
+    this.cfs.configObject = undefined
+    this.canRunDownloadDiscard = false
+  }
+
+  public onFileSelected(e: any) { 
+    console.log("onFileSelected")
+    const file: File = e.target.files[0] 
+    this.filename = file.name
+//  console.log(`app: onFileSelected(): filename=${this.filename}; subscribing to observable ...`)
+    const obs$ = this.readFileContentObs(file)
+    obs$.subscribe((fileContent: string) => { 
+      this.cfs.configAsJson = fileContent
+      this.cfs.configObject = JSON.parse(fileContent) 
+      //this.router.navigate(["../edit"], { relativeTo: this.route })
+      console.log(`config-file.service: cfs.configObject=`)
+      console.log(this.cfs.configObject)
+      this.canRunDownloadDiscard = true
+//    console.log(this.cfs.objFromJsonFile)
+    })
+  }
+
+  private readFileContentObs(file: File): Observable<string> {
+    return new Observable((subscriber) => {
+      if (!file) subscriber.error("no file selected")
+      if (file.size == 0) subscriber.error("selected file is empty")
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        if (!reader.result) subscriber.error("no result from reading")
+        else subscriber.next(reader.result.toString())
+      }
+      reader.onerror = (error) => subscriber.error(error)
+      reader.readAsText(file)
+    })
+  }
+
+  public onSaveFile(): void {
+    //  let fileContent = "Hi there, I was just saved from the Angular app!"
+    const fileContent = this.cfs.configObject
+    const file = new Blob([JSON.stringify(fileContent, null, "\t")], { type: "application/json" })
+    const link = document.createElement("a")
+    link.href = URL.createObjectURL(file)
+    link.download = this.filename
+    console.log("system.onSaveFile(): saving to " + link.download)
+    link.click()
+    link.remove()
+  }
+
+
+
+  changeHandler() {
+    console.log("Change Handler")
+  }
 }
