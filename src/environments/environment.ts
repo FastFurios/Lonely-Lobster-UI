bd6eefc2aad574e66375d38b19f17c563669c3d0
Gerold <gerold@lindorfer.com>
2025-03-08 16:27:39 +0100

    separated DEV and PROD frontend applications in ENtraID

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,17 +14,3 @@ export const environment = {
     redirectUri: 'http://localhost:4200' // change redirect url based on where u want to redirect after the authentication
   }
 }
-
-
-
-// https://stackoverflow.com/questions/47426721/angular-cli-change-rest-api-url-on-build
-
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
