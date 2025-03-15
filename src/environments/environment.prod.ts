bb2399ce2fbd375fd2724d100b4b3a63131929e6
Gerold <gerold@lindorfer.com>
2025-03-15 10:13:18 +0100

    download filename date fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
 
   // msal config details
   msalConfig: {
-    clientId: "65f67e5b-d0c0-4677-adf8-fc895254393a", // Azure Portal / Entra Overview / App Registration: Application (client) ID: it is the application id of this Angular "Lonely Lobster Frontend (DEV) 
-    authority: "https://login.microsoftonline.com/49bf30a4-54b2-47ae-b9b1-ffa71ed3d475",  // Azure Portal / Entra Overview / App Registration: Directory (tenant) ID i.e. my tenant id
+    clientId: "<use your own Azure IDP reference for PROD>", // Azure Portal / Entra Overview / App Registration: Application (client) ID: it is the application id of this Angular "Lonely Lobster Frontend (DEV) 
+    authority: "https://login.microsoftonline.com/<use your own Azure tenant>",  // Azure Portal / Entra Overview / App Registration: Directory (tenant) ID i.e. my tenant id
     redirectUri: 'https://lonely-lobster-ftdqhshte9abe3eq.germanywestcentral-01.azurewebsites.net' // change redirect url based on where u want to redirect after the authentication
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
 
   // msal config details
   msalConfig: {
-    clientId: "7d27668f-05e5-4bbf-a904-b97b2574c813", // Azure Portal / Entra Overview / App Registration: Application (client) ID: it is the application id of this Angular "Lonely Lobster Frontend (DEV) 
-    authority: "https://login.microsoftonline.com/49bf30a4-54b2-47ae-b9b1-ffa71ed3d475",  // Azure Portal / Entra Overview / App Registration: Directory (tenant) ID i.e. my tenant id
+    clientId: "<use your own Azure IDP reference for DEV>", // Azure Portal / Entra Overview / App Registration: Application (client) ID: it is the application id of this Angular "Lonely Lobster Frontend (DEV) 
+    authority: "https://login.microsoftonline.com/<use your own Azure tenant>",  // Azure Portal / Entra Overview / App Registration: Directory (tenant) ID i.e. my tenant id
     redirectUri: 'http://localhost:4200' // change redirect url based on where u want to redirect after the authentication
   }
 }
