a677e57ab4f44e342a1745ed11ecb358b0522cc9
Gerold <gerold@lindorfer.com>
2025-04-13 14:06:44 +0200

    assigned URL lonely-lobster.azurewebsites.net - step 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
 
   // msal config details
   msalConfig: {
-    clientId: "200c978a-9108-4e74-ae41-501e2d5a5ea1", // Azure Portal / Entra Overview / App Registration: Application (client) ID: it is the application id of this Angular "Lonely Lobster Frontend (DEV) 
+    clientId: "<use your own Azure IDP reference for PROD>", // Azure Portal / Entra Overview / App Registration: Application (client) ID: it is the application id of this Angular "Lonely Lobster Frontend (DEV) 
     authority: "https://login.microsoftonline.com/<use your own Azure tenant>",  // Azure Portal / Entra Overview / App Registration: Directory (tenant) ID i.e. my tenant id
     redirectUri: 'https://lonely-lobster.azurewebsites.net' // change redirect url based on where u want to redirect after the authentication
   }
