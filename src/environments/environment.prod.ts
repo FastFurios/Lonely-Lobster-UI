8ba3dd5d943057325970df85468dc88773c9ab45
Gerold <gerold@lindorfer.com>
2025-05-09 14:22:51 +0200

    Editor: added WIP optimizer from start checkbox

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   API_URL: "https://lonely-lobster.azurewebsites.net/",
-  version: "7.1.0",  // loading work order load files
+  version: "7.1.1",  // fix for issue #25 (wip limit optimization from start editable in Editor)
 
   // msal config details
   msalConfig: {
-    clientId: "<use your own Azure IDP reference for PROD>", // Azure Portal / Entra Overview / App Registration: Application (client) ID: it is the application id of this Angular "Lonely Lobster Frontend (DEV) 
-    authority: "https://login.microsoftonline.com/<use your own Azure tenant>",  // Azure Portal / Entra Overview / App Registration: Directory (tenant) ID i.e. my tenant id
+    clientId: "200c978a-9108-4e74-ae41-501e2d5a5ea1", // Azure Portal / Entra Overview / App Registration: Application (client) ID: it is the application id of this Angular "Lonely Lobster Frontend (DEV) 
+    authority: "https://login.microsoftonline.com/49bf30a4-54b2-47ae-b9b1-ffa71ed3d475",  // Azure Portal / Entra Overview / App Registration: Directory (tenant) ID i.e. my tenant id
     redirectUri: 'https://lonely-lobster.azurewebsites.net' // change redirect url based on where u want to redirect after the authentication
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  version: "7.1.0",  // loading work order load files
+  version: "7.1.1",  // fix for issue #25 (wip limit optimization from start editable in Editor)
   API_URL: "http://localhost:3000/",
 
   // msal config details
   msalConfig: {
-    clientId: "<use your own Azure IDP reference for DEV>", // Azure Portal / Entra Overview / App Registration: Application (client) ID: it is the application id of this Angular "Lonely Lobster Frontend (DEV) 
-    authority: "https://login.microsoftonline.com/<use your own Azure tenant>",  // Azure Portal / Entra Overview / App Registration: Directory (tenant) ID i.e. my tenant id
+    clientId: "7d27668f-05e5-4bbf-a904-b97b2574c813", // Azure Portal / Entra Overview / App Registration: Application (client) ID: it is the application id of this Angular "Lonely Lobster Frontend (DEV) 
+    authority: "https://login.microsoftonline.com/49bf30a4-54b2-47ae-b9b1-ffa71ed3d475",  // Azure Portal / Entra Overview / App Registration: Directory (tenant) ID i.e. my tenant id
     redirectUri: 'http://localhost:4200' // change redirect url based on where u want to redirect after the authentication
   }
 }
